68536d63907c21d995d978de59606242137c0080
mohamadlounnas <mohamadlounnas@gmail.com>
2024-11-12 21:33:11 +0100

    Enhance FlickPortraitControls with playback speed selection and UI adjustments

--- lib/src/controls/flick_portrait_controls.dart
+++ lib/src/controls/flick_portrait_controls.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flick_video_player/flick_video_player.dart';
+import 'package:provider/provider.dart';
 
 /// Default portrait controls.
 class FlickPortraitControls extends StatelessWidget {
   const FlickPortraitControls(
       {Key? key,
-      this.iconSize = 20,
-      this.fontSize = 12,
+      this.iconSize = 25,
+      this.fontSize = 14,
       this.progressBarSettings})
       : super(key: key);
 
@@ -25,6 +26,10 @@ class FlickPortraitControls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FlickControlManager controlManager =
+    Provider.of<FlickControlManager>(context);
+    FlickVideoManager videoManager = Provider.of<FlickVideoManager>(context);
+
     return Stack(
       children: <Widget>[
         Positioned.fill(
@@ -97,6 +102,67 @@ class FlickPortraitControls extends StatelessWidget {
                       FlickSubtitleToggle(
                         size: iconSize,
                       ),
+                      SizedBox(
+                        width: iconSize / 2,
+                      ),
+                      // icon for speed
+                      GestureDetector(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text('Select Speed'),
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    ListTile(
+                                      title: Text('0.25x'),
+                                      onTap: () {
+                                        controlManager.setPlaybackSpeed(0.25);
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                    ListTile(
+                                      title: Text('0.5x'),
+                                      onTap: () {
+                                        controlManager.setPlaybackSpeed(0.5);
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                    ListTile(
+                                      title: Text('1x'),
+                                      onTap: () {
+                                        controlManager.setPlaybackSpeed(1);
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                    ListTile(
+                                      title: Text('1.5x'),
+                                      onTap: () {
+                                        controlManager.setPlaybackSpeed(1.5);
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                    ListTile(
+                                      title: Text('2x'),
+                                      onTap: () {
+                                        controlManager.setPlaybackSpeed(2);
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        },
+                        child: Icon(
+                          Icons.play_circle_outline_sharp,
+                          size: iconSize,
+                        ),
+                      ),
+                      
                       SizedBox(
                         width: iconSize / 2,
                       ),
